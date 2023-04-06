aeded89f4216be36c8b31e63aeb82664a04a8f1d
MdeAssis-Adviters <129104996+MdeAssis-Adviters@users.noreply.github.com>
2023-04-06 18:26:42 -0300

    comit maicon
    nada a declarar

--- src/pages/Licencia/Licencia.jsx
+++ src/pages/Licencia/Licencia.jsx
@@ -1,49 +1,103 @@
 import React from "react";
-import { Box, Avatar, Typography, Stack, Button } from "@mui/material";
+import { Box, Avatar, Typography, Stack, Button, FormControl, Select, MenuItem } from "@mui/material";
+import MiniCalendariio from "../../Components/MiniCalendario/MiniCalendario";
 
 const Licencia = () => {
-  // obter a imagem do usuário da base de dados
-  // const userImage = ...
+  const [tipoLicencia, setTipoLicencia] = React.useState('');
 
-  // obter o número de dias disponíveis para solicitar da base de dados
-  // const daysLeft = ...
-
-  // obter o estado da solicitação da base de dados
-  // const requestStatus = ...
+  const handleChange = (event) => {
+    setTipoLicencia(event.target.value);
+  };
 
   return (
-    <Stack direction="row" spacing={2} justifyContent="space-around">
-      <Box sx={{ display: "flex" }}>
-        {/* Foto e nome do usuário */}
-        <Box sx={{ display: "flex", alignItems: "center", margin: "20px" }}>
-          <Avatar
-            sx={{ width: 64, height: 64, marginRight: "10px" }}
-            alt="Foto do usuário"
-          />
-          <Typography variant="h6">Maicon</Typography>
+    <>
+      <Stack direction="row" spacing={2} justifyContent="space-around">
+        <Box sx={{
+          display: "flex", border: '0.5px solid black', width: "90%", justifyContent: "space-between",
+          alignItems: "center", paddingLeft: '5px', paddingRight: '10px'
+        }}>
+          {/* Foto e nome do usuário */}
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Avatar
+              sx={{ width: 64, height: 64, marginRight: "10px" }}
+              alt="Foto do usuário" />
+            <Typography variant="h6">Maicon</Typography>
+          </Box>
+          {/* Título "Balance actual" y número de dias disponíbles */}
+          <Box sx={{ display: "flex", alignItems: "center", margin: "20px" }}>
+            <Typography variant="h6">Balance actual</Typography>
+            <Typography variant="subtitle1" sx={{ marginLeft: "10px" }}>
+              24 dias
+            </Typography>
+          </Box>
+
+          {/* Título "Estado" e status da solicitação */}
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+
+            }}
+          >
+            <Typography variant="h6">Estado</Typography>
+            <Typography
+              variant="subtitle1"
+              sx={{
+                background: "#05CB3C",
+                borderRadius: "16px",
+                color: "white",
+                width: "164px",
+                height: "19px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {"AUN NO ENVIADO"}
+            </Typography>
+          </Box>
         </Box>
-        {/* Título "Balance actual" y número de dias disponíbles */}
-        <Box sx={{ display: "flex", alignItems: "center", margin: "20px" }}>
-          <Typography variant="h6">Balance actual</Typography>
-          <Typography variant="subtitle1" sx={{ marginLeft: "10px" }}>
-            24 dias
+      </Stack>
+
+      <Box sx={{ display: "flex", border: '0.5px solid black', width: "90%", justifyContent: "space-between",
+          alignItems: "center", paddingLeft: '5px', paddingRight: '10px'}}>
+        <Stack direction="column" alignItems="flex-start" marginTop="20px">
+          <Typography variant="subtitle1" sx={{ fontFamily: "Roboto", fontSize: "14px", color: "#797979" }}>
+            TIPO DE LICENCIA
           </Typography>
-        </Box>
+          <Box sx={{ width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center", paddingLeft: '5px', paddingRight: '10px', marginBottom: '20px' }}>
+            <FormControl sx={{ minWidth: 120 }}>
+              <Select value={tipoLicencia} onChange={handleChange}>
+                <MenuItem value="licencia" disabled selected>Licencia</MenuItem>
+                <MenuItem value="vacaciones">Vacaciones</MenuItem>
+                <MenuItem value="enfermedad">Enfermedad</MenuItem>
+                <MenuItem value="examen">Examen</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Stack>
+
+        <Stack direction="row" spacing={0} justifyContent="space-evenly">
+          <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Typography variant="subtitle1">Desde</Typography>
+            <MiniCalendariio />
+          </Box>
+          <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Typography variant="subtitle1">Hasta</Typography>
+            <MiniCalendariio />
+          </Box>
+        </Stack>
       </Box>
-      {/* Título "Estado" e status da solicitação */}
-      <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-        <Typography variant="h6">Estado</Typography>
-        <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-          <Typography variant="subtitle1" sx={{ background: "#05CB3C", borderRadius: "16px", color: "white" }}>{"AUN NO ENVIADO"}</Typography>
-        </Box>
-      </Box>
+
       {/* Botão de solicitação de aprovação */}
       <Box sx={{ position: "absolute", bottom: "20px", right: "20px" }}>
         <Button variant="contained" color="primary" size="large">
           Solicitar Aprobacion
         </Button>
       </Box>
-    </Stack>
+    </>
   );
 };
 
