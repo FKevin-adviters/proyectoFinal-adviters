7fd403d861dcef0f4736cee35b3ee797c591be69
mezequiel-adviters <capacitaciones12@adviters.com>
2023-04-20 11:26:40 -0300

    Feriados/licencias

--- src/pages/Calendario/Calendario.jsx
+++ src/pages/Calendario/Calendario.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 import { Box, Typography } from "@mui/material";
 import Feriados from "./Feriados";
 import "./feriados.css"
+import CalendarioPrueba from "./CalendarioPrueba";
 
 const Calendario = () => {
+  const [feriados, setFeriados] = useState([
+    { fecha: "01/05/2023", motivo: "Dia del trabajador" },
+    { fecha: "25/05/2023", motivo: "Revolucion de mayo" },
+    { fecha: "30/05/2023", motivo: "Bailo chamame" },
+  ]);
+
   return (
     <Box id="calendarioFeriados"
-    sx={{ display: "flex" }}>
+    sx={{ display: "flex"}}>
       <Box
         sx={{
           padding: "20px",
@@ -19,10 +26,16 @@ const Calendario = () => {
         }}
       >
         <Typography variant="h4" color={"red"}>
-          FERIADOS
+          Feriados
         </Typography>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateCalendar />
+          {/* <DateCalendar
+            slotProps={{
+              day: {
+                highlightedDays: [1, 2, 15]
+              } 
+            }}/> */}
+            <CalendarioPrueba/>
         </LocalizationProvider>
       </Box>
       <Box
